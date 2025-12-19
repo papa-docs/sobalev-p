5fa6eee4c8d22a81d87e3f7d75813125256dc074
papa-docs <papa-docs1@proton.me>
2025-12-19 15:53:03 +0200

    Add files via upload

---
+++ FAKE/values.js
@@ -0,0 +1,81 @@
+// values.js
+
+// Основные данные 
+var fio               = "Шевченко Вiталiй Андрiйович";
+var fio_en            = "Shevchenko Vitalii";
+var birth             = "08.02.2005"; //дата рождения
+var date_give         = "14.07.2020"; //Дата видачи
+var date_out          = "14.07.2024"; // действителен до
+var organ             = "4634"; //орган что выдал документ
+var rnokpp            = "3839007473"; //ИНН
+var uznr              = "20050208-10257"; //Номер записи
+var pass_number       = "004951255"; //номер паспорта
+
+var registeredOn      = "05.04.2005"; //дата регистрации
+
+// Прописка
+var live              = "М. ЛЬВIВ"; //Место рождение 
+var legalAdress       = "УКРАЇНА ЛЬВIВСЬКА ОБЛАСТЬ М. САМБIР ПЛ. РИНОУ БУД. 33 КВ. 4"; //Место проживание
+var bank_adress       = "УКРАЇНА ЛЬВIВСЬКА ОБЛАСТЬ М. САМБIР ПЛ. РИНОУ БУД. 33 КВ. 4"; //Место жительства указано в банке
+
+var sex               = "Ч";
+var sex_en            = "M";
+
+// Данные для Прав
+var rights_categories = "B"; //Категории
+var prava_number      = "AUK798327"; // номер прав
+var prava_date_give   = "01.11.2025"; //Дата выдачи Прав
+var prava_date_out    = "01.11.2027"; //Действителен ДО
+var pravaOrgan        = "4634"; //орган который выдал
+
+// заграник
+var zagran_number     = "GE904689"; //номер загран
+var dateGiveZ         = "24.01.2023"; //выдан загран
+var dateOutZ          = "24.01.2033"; //коньчаеться загран
+
+
+
+var university        = "ХНУ имени Каразина"; // Університет
+var fakultet          = "Физико-технический"; // Факультет
+var stepen_dip        = "Магістра";
+var univer_dip        = "ХНУ имени Каразина";
+var dayout_dip        = "01.07.2023";
+var special_dip       = "Прикладная математика";
+var number_dip        = "MT-545678";
+var form              = "Очная";
+
+var student_number    = "2022154258";
+var student_date_give = "01.09.2021";
+var student_date_out  = "30.06.2025";
+
+
+
+// Включение/выключение документов
+var isRightsEnabled   = true;
+var isZagranEnabled   = true;
+var isDiplomaEnabled  = false;
+var isStudyEnabled    = false;
+// Добавь остальные переменные по аналогии
+
+// Пути к нужным фото    ?timestamp=N   "photo.jpg?timestamp=N" динамически: var url = "photo.jpg?timestamp=" + N;
+
+var photo_passport = "2.png"; //эДок, паспорт,
+var photo_rights   = "1.png"; //права
+var photo_students = "1.png"; //диплом, студ
+var photo_zagran   = "3.png"; //загран
+
+var signPng           = "sign.png"; //подпись
+// Можно добавить еще, если надо
+
+
+
+
+
+
+
+
+
+
+
+
+
